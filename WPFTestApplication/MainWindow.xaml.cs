7b3f1a099e867b18f191941f543e04c43438e8f9
mordashov <mordashovv@gmail.com>
2018-10-07 22:25:03 +0300

    MapControl test

--- WPFTestApplication/MainWindow.xaml.cs
+++ WPFTestApplication/MainWindow.xaml.cs
@@ -1,28 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Globalization;
+using System.Threading;
+using Microsoft.Maps.MapControl.WPF;
+using Microsoft.Maps.MapControl.WPF.Design;
 
 namespace WPFTestApplication
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class SwitchMapViews : Window
     {
-        public MainWindow()
+        LocationConverter locConverter = new LocationConverter();
+
+        public SwitchMapViews()
         {
             InitializeComponent();
+            //Set focus on the map
+            myMap.Focus();
+            // Displays the current latitude and longitude as the map animates.
+            myMap.ViewChangeOnFrame += new EventHandler<MapEventArgs>(viewMap_ViewChangeOnFrame);
+            // The default animation level: navigate between different map locations.
+            //viewMap.AnimationLevel = AnimationLevel.Full;
+        }
+
+        private void viewMap_ViewChangeOnFrame(object sender, MapEventArgs e)
+        {
+            // Gets the map object that raised this event.
+            Map map = sender as Map;
+            // Determine if we have a valid map object.
+            if (map != null)
+            {
+                // Gets the center of the current map view for this particular frame.
+                Location mapCenter = map.Center;
+
+                // Updates the latitude and longitude values, in real time,
+                // as the map animates to the new location.
+                txtLatitude.Text = string.Format(CultureInfo.InvariantCulture, "{0:F5}", mapCenter.Latitude);
+                txtLongitude.Text = string.Format(CultureInfo.InvariantCulture, "{0:F5}", mapCenter.Longitude);
+            }
+        }
+
+        private void ChangeMapView_Click(object sender, RoutedEventArgs e)
+        {
+            // Parse the information of the button's Tag property
+            string[] tagInfo = ((Button)sender).Tag.ToString().Split(' ');
+            Location center = (Location)locConverter.ConvertFrom(tagInfo[0]);
+            //double zoom = System.Convert.ToDouble(tagInfo[1]);
+            double zoom = double.Parse(tagInfo[1].ToString(), CultureInfo.InvariantCulture);
+
+            // Set the map view
+            myMap.SetView(center, zoom);
+
+        }
+
+        private void AnimationLevel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem cbi = (ComboBoxItem)(((ComboBox)sender).SelectedItem);
+            string v = cbi.Content as string;
+            if (!string.IsNullOrEmpty(v) && myMap != null)
+            {
+                AnimationLevel newLevel = (AnimationLevel)Enum.Parse(typeof(AnimationLevel), v, true);
+                myMap.AnimationLevel = newLevel;
+            }
         }
     }
 }
